e2e2233d69d0c86ab80447747f9a82f48c43274c
Ido Ben-Hur <idoybh2@gmail.com>
2023-12-25 18:33:16 +0200

    DeviceSettings: Animate AlertSliderDialog movements
    And better handle timeout, fixed a log spam for doze intent

--- KeyHandler/src/org/lineageos/settings/device/AlertSliderPlugin.kt
+++ KeyHandler/src/org/lineageos/settings/device/AlertSliderPlugin.kt
@@ -16,7 +16,7 @@ import android.hardware.display.AmbientDisplayConfiguration
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
-import android.os.UserHandle;
+import android.os.UserHandle
 import android.view.View
 import com.android.systemui.plugins.OverlayPlugin
 import com.android.systemui.plugins.annotations.Requires
@@ -81,7 +81,6 @@ class AlertSliderPlugin : OverlayPlugin {
                         removeMessages(MSG_DIALOG_SHOW)
                         removeMessages(MSG_DIALOG_DISMISS)
                         removeMessages(MSG_DIALOG_RESET)
-                        removeMessages(MSG_DIALOG_UPDATE)
 
                         // Show/hide dialog
                         if (value) {
@@ -124,6 +123,7 @@ class AlertSliderPlugin : OverlayPlugin {
         private fun handleUpdate(info: NotificationInfo) {
             synchronized (dialogLock) {
                 if (maybeRemake()) showing = true
+                handleResetTimeout()
                 handleDoze()
                 dialog.setState(info.position, info.mode)
             }
