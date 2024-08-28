LineageOS/android_hardware_oplus

Introduce vendor.oplus.hardware.osense.client-service
Change-Id: I5b9e1324a70966e1505e45fca54273ea22028be5

aidl/osense-client/OsenseAidlHalReporter.cpp
@@ -1,0 +1,52 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "OsenseAidlHalReporter.h"
+
+namespace aidl {
+namespace vendor {
+namespace oplus {
+namespace hardware {
+namespace osense {
+namespace client {
+
+ndk::ScopedAStatus OsenseAidlHalReporter::checkAccessPermission(const std::string& in_identity,
+                                                                int32_t* _aidl_return) {
+    *_aidl_return = 1;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseClrSceneAction(const std::string& in_identity,
+                                                               int64_t in_request) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseSetNotification(
+        const std::string& in_identity,
+        const OsenseAidlHalNotifyRequest& in_osenseHalNotifyRequest) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseSetSceneAction(
+        const std::string& in_identity, const OsenseAidlHalSaRequest& in_osenseHalSaRequest) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseResetCtrlData(const std::string& in_identity) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseSetCtrlData(const std::string& in_identity,
+                                                            const OsenseControlInfo& in_ctrldata) {
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace client
+}  // namespace osense
+}  // namespace hardware
+}  // namespace oplus
+}  // namespace vendor
+}  // namespace aidl

aidl/osense-client/OsenseAidlHalReporter.h
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/oplus/hardware/osense/client/BnOsenseAidlHalReporter.h>
+
+namespace aidl {
+namespace vendor {
+namespace oplus {
+namespace hardware {
+namespace osense {
+namespace client {
+
+struct OsenseAidlHalReporter : public BnOsenseAidlHalReporter {
+    ndk::ScopedAStatus checkAccessPermission(const std::string& in_identity,
+                                             int32_t* _aidl_return) final;
+    ndk::ScopedAStatus osenseClrSceneAction(const std::string& in_identity,
+                                            int64_t in_request) final;
+    ndk::ScopedAStatus osenseSetNotification(
+            const std::string& in_identity,
+            const OsenseAidlHalNotifyRequest& in_osenseHalNotifyRequest) final;
+    ndk::ScopedAStatus osenseSetSceneAction(
+            const std::string& in_identity,
+            const OsenseAidlHalSaRequest& in_osenseHalSaRequest) final;
+    ndk::ScopedAStatus osenseResetCtrlData(const std::string& in_identity) final;
+    ndk::ScopedAStatus osenseSetCtrlData(const std::string& in_identity,
+                                         const OsenseControlInfo& in_ctrldata) final;
+};
+
+}  // namespace client
+}  // namespace osense
+}  // namespace hardware
+}  // namespace oplus
+}  // namespace vendor
+}  // namespace aidl

yaap/hardware_oplus

aidl/osense-client/OsenseAidlHalReporter.cpp
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "OsenseAidlHalReporter.h"
+
+namespace aidl {
+namespace vendor {
+namespace oplus {
+namespace hardware {
+namespace osense {
+namespace client {
+
+ndk::ScopedAStatus OsenseAidlHalReporter::checkAccessPermission(const std::string& identity,
+                                                                int32_t* _aidl_return) {
+    *_aidl_return = 1;
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseClrSceneAction(const std::string& identity,
+                                                               int64_t request) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseSetNotification(
+        const std::string& identity, const OsenseAidlHalNotifyRequest& osenseHalNotifyRequest) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseSetSceneAction(
+        const std::string& identity, const OsenseAidlHalSaRequest& osenseHalSaRequest) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseResetCtrlData(const std::string& identity) {
+    return ndk::ScopedAStatus::ok();
+}
+
+ndk::ScopedAStatus OsenseAidlHalReporter::osenseSetCtrlData(const std::string& identity,
+                                                            const OsenseControlInfo& ctrldata) {
+    return ndk::ScopedAStatus::ok();
+}
+
+}  // namespace client
+}  // namespace osense
+}  // namespace hardware
+}  // namespace oplus
+}  // namespace vendor
+}  // namespace aidl

aidl/osense-client/OsenseAidlHalReporter.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2024 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include <aidl/vendor/oplus/hardware/osense/client/BnOsenseAidlHalReporter.h>
+
+namespace aidl {
+namespace vendor {
+namespace oplus {
+namespace hardware {
+namespace osense {
+namespace client {
+
+struct OsenseAidlHalReporter : public BnOsenseAidlHalReporter {
+    ndk::ScopedAStatus checkAccessPermission(const std::string& identity,
+                                             int32_t* _aidl_return) final;
+    ndk::ScopedAStatus osenseClrSceneAction(const std::string& identity, int64_t request) final;
+    ndk::ScopedAStatus osenseSetNotification(
+            const std::string& identity,
+            const OsenseAidlHalNotifyRequest& osenseHalNotifyRequest) final;
+    ndk::ScopedAStatus osenseSetSceneAction(const std::string& identity,
+                                            const OsenseAidlHalSaRequest& osenseHalSaRequest) final;
+    ndk::ScopedAStatus osenseResetCtrlData(const std::string& identity) final;
+    ndk::ScopedAStatus osenseSetCtrlData(const std::string& identity,
+                                         const OsenseControlInfo& ctrldata) final;
+};
+
+}  // namespace client
+}  // namespace osense
+}  // namespace hardware
+}  // namespace oplus
+}  // namespace vendor
+}  // namespace aidl